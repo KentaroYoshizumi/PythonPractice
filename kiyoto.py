a17207e8a071feff3c850933041e9579ad412be4
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-12 04:16:43 +0900

    Create kiyoto.py

---
+++ kiyoto.py
@@ -0,0 +1,8 @@
+kiyoto = 17870400
+kiyoto_hour = kiyoto / 60
+kiyoto_days = kiyoto_hour / 24
+kiyoto_month = kiyoto_days / 30
+kiyoto_year = kiyoto_month / 12
+
+print (kiyoto_year)
+print (kiyoto_month)
