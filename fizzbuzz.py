62224695e70574791448b47f1077ad20398208fb
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-17 01:33:55 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,14 +1,9 @@
-def fizzbuzz(i):
-    return "Fizz" * (i % 3 == 0) + "Buzz" * (i % 5 == 0) or str(i)
-  
-      if i % 15 == 0:
-         return "FizzBuzz"
-      elif i % 3 == 0:
-         return "Fizz"
-      elif i % 5 == 0:
-         return "Buzz"
-      else:
-        return str(i)
-      
-    outputs = [fizzbuzz(i) for i in range(1, 101)]
-    print(outputs)
+for i in range (1, 101):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
