1f677ecd1df1c7f694bbeeda6b3a2791967fa0fb
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-18 02:02:28 +0900

    Update janken.py

--- janken.py
+++ janken.py
@@ -4,27 +4,36 @@
 com = 0
 you = 1
 
-if com == 0:
-    if you == 0:
-        result = "あいこ"
-    elif you == 1:
-        result = "負け"
-    else:
-        result = "勝ち"
-elif com == 1:
-    if you == 0:
-        result = "勝ち"
-    elif you == 1:
-        result = "あいこ"
-    else:
-        result = "負け"
-else:
-    if you == 0:
-        result = "負け"
-    elif you == 1:
-        result = "勝ち"
-    else:
-        result = "あいこ"
+if com == you:
+    result = "あいこ"
+elif com == 0 && you == 2　or \
+     com == 1 && you == 0 or \
+     com == 2 && you == 1:
+    result = "勝ち"
+#else:
+#    result = "負け"
+
+#if com == 0:
+#    if you == 0:
+#        result = "あいこ"
+#    elif you == 1:
+#        result = "負け"
+#    else:
+#        result = "勝ち"
+#elif com == 1:
+#    if you == 0:
+#        result = "勝ち"
+#    elif you == 1:
+#        result = "あいこ"
+#    else:
+#        result = "負け"
+#else:
+#    if you == 0:
+#        result = "負け"
+#    elif you == 1:
+#        result = "勝ち"
+#    else:
+#        result = "あいこ"
         
 te = ["グー", "チョキ", "パー"]        
         
