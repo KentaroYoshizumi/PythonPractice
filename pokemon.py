e65a83ec7106c9ba3c9e2e17834edfad90763a10
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-19 23:45:53 +0900

    Update pokemon.py

--- pokemon.py
+++ pokemon.py
@@ -4,3 +4,13 @@ class Pokemon:
      self.name = name
      self.heart = heart
      self.attack = attack
+
+Pikachu = Pokemon("Pikachu", 30, 13)
+Zenigame = Pokemon("Zenigame", 50, 6)
+
+def battle(Pikachu, Zenigame)
+    print("********")
+    print("戦闘開始")
+    print("********")
+    
+    
