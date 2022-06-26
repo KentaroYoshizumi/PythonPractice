c0a91e75ae66145838bc9fa9ae44f50c118895b2
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-26 10:49:18 +0900

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,4 @@
+#Python3系では動かない
 author = "Kafka"
 
 author[0]
@@ -5,3 +6,12 @@ author[1]
 author[2]
 author[3]
 author[4]
+
+#3系はこっち
+author = "Kafka"
+
+print(author[0])
+print(author[1])
+print(author[2])
+print(author[3])
+print(author[4])
