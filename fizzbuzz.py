f5cdada78bd70da445237918c04c00ebd5a75087
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-28 00:34:25 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -7,3 +7,16 @@ for i in range (1, 101):
         print("Buzz")
     else:
         print(i)
+
+#fizzbuzzsum
+n = int(input())
+
+fizzbuzz_sum = 0
+
+for i in range(1, n+1):
+    if i % 3 == 0 or i % 5 == 0:
+        continue
+    else:
+        fizzbuzz_sum += i
+
+print(fizzbuzz_sum)
