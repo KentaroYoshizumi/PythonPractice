22d1a83278f2619e21bfcb0bff9ab770a4ca450e
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-28 23:18:30 +0900

    Create class.py

---
+++ class.py
@@ -0,0 +1,11 @@
+class Person:
+
+    def __init__(self, name, nationality, age):
+        self.name = name
+        self.nationality = nationality
+        self.age = age
+
+
+# インスタンス化
+yoshizumi = Person('吉積', '日本', 30)
+mike = Person('mike', 'America'13)
