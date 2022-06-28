158e8026fef9f46a336d352b33aff8b539f4ad51
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-28 23:51:45 +0900

    Update class.py

--- class.py
+++ class.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 class Person:
 
     def __init__(self, name, nationality, age):
@@ -5,7 +6,12 @@ class Person:
         self.nationality = nationality
         self.age = age
 
+    def say_hello(self, name):
+        print('こんにちは、{}さん。私は{}です。'.format(name, self.name))
+
 
 # インスタンス化
 yoshizumi = Person('吉積', '日本', 30)
-mike = Person('mike', 'America'13)
+mike = Person('mike', 'America', 13)
+
+print(mike.say_hello('佐藤'))
