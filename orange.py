826684dac5bdddf59118e24d57f4e2c4af955844
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-06-29 14:26:07 +0900

    Create orange.py

---
+++ orange.py
@@ -0,0 +1,10 @@
+class Orange:
+    def __init__(self, weight, color):
+        self.weight = weight
+        self.color = color
+        print("Created!")
+
+
+orange1 = Orange(50, "red")
+print(orange1.weight)
+print(orange1.color)
