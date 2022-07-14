9ab24b0bd0dbe41de574c0512dd5405d8a948543
KentaroYoshizumi <39523342+KentaroYoshizumi@users.noreply.github.com>
2022-07-14 18:51:37 +0900

    Update Health.py

--- Health.py
+++ Health.py
@@ -1,18 +1,21 @@
 class Health:
+
     def __init__(self, name):
         self.name = name
 
     def judge(self, height):
-        if (height >= 160):
-            result = '大きいさん'
+        if height >= 160:
+            result = "大きいさん"
+            return result
+
         else:
-            result = '小さいさん'
-        return result
+            result = "小さいさん"
+            return result
 
 
-name = str(input("名前は？: "))
-height = float(input("身長(cm)は？: "))
-a001 = Health(name)
-result = a001.judge(height)
-print("名前: " + a001.name)
+name = str(input("名前は？："))
+height = float(input("身長(cm)は？："))
+user01 = Health(name)
+result = user01.judge(height)
+print("名前: " + user01.name)
 print("あなたは: " + result)
